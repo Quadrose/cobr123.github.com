1dcd520c2e10b4215878a58d57980d0c818bbd0b
cobr123 <cobr123@users.noreply.github.com>
2016-02-25 15:17:02 +0300

    Update script.js
    Former-commit-id: 71244b6bb1159ee88d78b63d2abca0e18ace1bb0

--- by_trade_at_cities/script.js
+++ by_trade_at_cities/script.js
@@ -147,7 +147,7 @@ function loadPrediction(predRow) {
 			    maxCnt -= 1;
 			    uniqPred[key] = 1;
 				output += '<tr class="trec hoverable">';
-				output += '<td align="right" id="td_sellVolume">'+getVolume(val.sv, locale)+'</td>';
+				output += '<td align="right" id="td_sellVolume">'+getVolume(val.sv, locale)+' ('+Math.round(parseFloat(val.sv.replace(/[\D]+/g,''))/parseFloat(val.mv)*100)+'%)</td>';
 				output += '<td align="right" id="td_price">'+parseFloat(val.p).toFixed(2)+'</td>';
 				output += '<td align="right" id="td_quality">'+parseFloat(val.q).toFixed(2)+'</td>';
 				output += '<td align="right" id="td_brand">'+parseFloat(val.b).toFixed(2)+'</td>';
