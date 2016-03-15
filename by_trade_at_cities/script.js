4c62674f35be8eb3ffd5154a399dbfc726bcb6cb
safebuster <safebuster2@gmail.com>
2016-03-15 10:31:20 +0300

    ref
    Former-commit-id: 8aa4efb8c7d70a70ce1bf3a4aaf381f339ccaa8b

--- by_trade_at_cities/script.js
+++ by_trade_at_cities/script.js
@@ -275,7 +275,7 @@ function loadSavedFlt(){
 	if (sort_dir != null || sort_dir != '') {
 	    $('#sort_dir').val(sort_dir);
 	}
-	if (getVal('locale') == '' && (document.referrer === 'http://virtonomics.com/' || document.referrer === 'http://virtonomics-free.blogspot.ca/')) {
+	if (getVal('locale') == '' && (document.referrer.substring(0, 'http://virtonomics.com/'.length) === 'http://virtonomics.com/' || document.referrer.substring(0, 'http://virtonomics-free.blogspot.ca/'.length) === 'http://virtonomics-free.blogspot.ca/')) {
 	    setVal('locale', 'en');
 	}
 	
