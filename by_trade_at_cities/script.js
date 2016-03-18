8920bc63d89c951f91539bdaf2ab2e1328d1e56d
safebuster <safebuster2@gmail.com>
2016-03-18 10:21:28 +0300

    sagInvisibibleColumns
    Former-commit-id: bec5a6f567c6c2c2834081a025a535cf6e7c0c17

--- by_trade_at_cities/script.js
+++ by_trade_at_cities/script.js
@@ -696,19 +696,6 @@ function hideAllCol(){
 function initShowHideColSelect() {
     var show_hide_col = (getLocale() === 'en') ? $("select#show_hide_col_en").multiselect() : $("select#show_hide_col_ru").multiselect();
 
-    sagInvisibibleColumns = getVal('invisibible_columns_btac');
-    if (sagInvisibibleColumns == null) {
-        sagInvisibibleColumns = [];
-    } else {
-        $.each(sagInvisibibleColumns, function (key, val) {
-//            console.log('key = '+key +', val = '+val);
-            show_hide_col.multiselect("widget").find(":radio[value='"+val+"']").each(function() {
-                 this.click();
-            });
-        });
-        show_hide_col.multiselect('refresh');
-    }
-
     show_hide_col.multiselect({
         click: function(event, ui){
             if (ui.checked) {
@@ -725,6 +712,19 @@ function initShowHideColSelect() {
             hideAllCol();
         }
     });
+
+    sagInvisibibleColumns = getVal('invisibible_columns_btac');
+    if (sagInvisibibleColumns == null) {
+        sagInvisibibleColumns = [];
+    } else {
+        $.each(sagInvisibibleColumns, function (key, val) {
+//            console.log('key = '+key +', val = '+val);
+            show_hide_col.multiselect("widget").find(":radio[value='"+val+"']").each(function() {
+                 this.click();
+            });
+        });
+        show_hide_col.multiselect('refresh');
+    }
 }
 //////////////////////////////////////////////////////
 $(document).ready(function () {
