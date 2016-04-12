11c4293662bbe54631be01130669e8539791b59e
safebuster <safebuster2@gmail.com>
2016-04-12 18:18:27 +0300

    sortTableFunc
    Former-commit-id: 94e4861e97f6573265ef90b6f100e6e51c9bba4a

--- industry/script.js
+++ industry/script.js
@@ -168,6 +168,9 @@ function addToResultCache(val){
 	}
 }
 function sortTableFunc(a, b){
+	if (a === null) return -1;
+	if (b === null) return 1;
+
 	var numA = parseFloat(a.replace(',', '').replace('$', '').replace(/\s+/g,''), 10);
 	var numB = parseFloat(b.replace(',', '').replace('$', '').replace(/\s+/g,''), 10);
 	return numA === numB ? 0 : (numA > numB ? 1 : -1);
