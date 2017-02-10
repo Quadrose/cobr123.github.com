5a88a8b39aace084728bd00c0fc07e2b35108692
cobr123 <cobr123@users.noreply.github.com>
2017-02-10 18:02:49 +0300

    Update script.js

--- retail_trends/script.js
+++ retail_trends/script.js
@@ -962,9 +962,9 @@ $(document).ready(function () {
     });
     //loadSavedFlt(urlParams);
     $( "#from" ).datepicker( $.datepicker.regional[ $( "#locale" ).val() ] );
-    $( "#from" ).datepicker( "option", "dateFormat", "mm.dd.yy" );
+    $( "#from" ).datepicker( "option", "dateFormat", "dd.mm.yy" );
     $( "#to"   ).datepicker( $.datepicker.regional[ $( "#locale" ).val() ] );
-    $( "#to"   ).datepicker( "option", "dateFormat", "mm.dd.yy" );
+    $( "#to"   ).datepicker( "option", "dateFormat", "dd.mm.yy" );
       
     if (getLocale() != 'ru') {
         $('#locale').val(getLocale());
