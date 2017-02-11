93330e7d4e170191d081ccf538bb74ded2a00a1a
cobr123 <cobr123@users.noreply.github.com>
2017-02-11 21:44:24 +0300

    Update script.js

--- retail_trends/script.js
+++ retail_trends/script.js
@@ -226,7 +226,6 @@ function showTrendGraph(data) {
     return new Date(parseInt(dateParts[2]), parseInt(dateParts[1]) - 1, parseInt(dateParts[0]));
   }
   var defaultDateTo = new Date();
-  defaultDateTo.setMonth(defaultDateTo.getMonth() + 1);
   var dateTo = defaultDateTo;
   var defaultDateFrom = new Date();
   var dateFrom = defaultDateFrom;
