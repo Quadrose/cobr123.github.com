97bc590115c1da6b026570c99e073ac7cd5dd63c
cobr123 <cobr123@users.noreply.github.com>
2017-03-10 18:31:42 +0300

    Update script.js

--- predict_retail_sales/script.js
+++ predict_retail_sales/script.js
@@ -170,6 +170,7 @@ function tableSortFunc(spColId, a,b){
 			return cellValA === cellValB ? 0 : (cellValA > cellValB ? 1 : -1);
 		}
 }
+var coefficients = {"name": "PRICE","attrs": [{"name": "AVERAGE_SALARY","coef": 0.0122,"values": []},{"name": "LOCAL_PRICE","coef": 0.6621,"values": []},{"name": "LOCAL_QUALITY","coef": -0.4266,"values": []},{"name": "SHOP_SIZE","coef": 7.8021,"values": ["2","1","5","4","100000","1000","10000"]},{"name": "SHOP_SIZE","coef": -4.7147,"values": ["1000","10000"]},{"name": "TOWN_DISTRICT","coef": 7.6715,"values": ["Фешенебельный район"]},{"name": "DEPARTMENT_COUNT","coef": 5.4658,"values": ["3","9","7","5","6","1","8"]},{"name": "DEPARTMENT_COUNT","coef": -4.4476,"values": ["9","7","5","6","1","8"]},{"name": "DEPARTMENT_COUNT","coef": -32.4229,"values": ["7","5","6","1","8"]},{"name": "DEPARTMENT_COUNT","coef": 25.8548,"values": ["5","6","1","8"]},{"name": "DEPARTMENT_COUNT","coef": 8.7145,"values": ["6","1","8"]},{"name": "DEPARTMENT_COUNT","coef": 5.1224,"values": ["1","8"]},{"name": "DEPARTMENT_COUNT","coef": -17.9878,"values": ["8"]},{"name": "BRAND","coef": 2.7188,"values": []},{"name": "QUALITY","coef": 0.2904,"values": []},{"name": "NOTORIETY","coef": 0.0195,"values": []},{"name": "VISITORS_COUNT","coef": 3.8787,"values": ["около 500","более 1 000","более 20 000","менее 50","около 10 000","около 100","около 50 000","более 1 000 000","более 200 000","около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 9.6383,"values": ["около 100","около 50 000","более 1 000 000","более 200 000","около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": -20.1504,"values": ["около 50 000","более 1 000 000","более 200 000","около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 10.6222,"values": ["более 1 000 000","более 200 000","около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": -7.5533,"values": ["более 200 000","около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 4.0339,"values": ["около 5 000","более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": -8.0725,"values": ["более 100 000","более 30 000","около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 12.4751,"values": ["около 1 000 000","более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 6.9921,"values": ["более 2 000","более 300 000","около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 9.8393,"values": ["около 1 000","около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": -12.725,"values": ["около 500 000","более 300","более 2 000 000"]},{"name": "VISITORS_COUNT","coef": 30.7286,"values": ["более 300","более 2 000 000"]},{"name": "SERVICE_LEVEL","coef": -12.9559,"values": ["Низкий","Высокий","Элитный","Нормальный","Очень высокий"]},{"name": "SERVICE_LEVEL","coef": 12.7172,"values": ["Высокий","Элитный","Нормальный","Очень высокий"]},{"name": "SELLER_COUNT","coef": 6.9073,"values": ["27","17","10","31","28","25","13","21","8","14","5","6","7","4","50","51","41","47","43","45","46","44","40","26","37","52","39","30","29","33","32","0","34","36","3","2","1"]},{"name": "SELLER_COUNT","coef": -6.2837,"values": ["17","10","31","28","25","13","21","8","14","5","6","7","4","50","51","41","47","43","45","46","44","40","26","37","52","39","30","29","33","32","0","34","36","3","2","1"]},{"name": "SELLER_COUNT","coef": -3.6653,"values": ["31","28","25","13","21","8","14","5","6","7","4","50","51","41","47","43","45","46","44","40","26","37","52","39","30","29","33","32","0","34","36","3","2","1"]},{"name": "SELLER_COUNT","coef": 10.0576,"values": ["28","25","13","21","8","14","5","6","7","4","50","51","41","47","43","45","46","44","40","26","37","52","39","30","29","33","32","0","34","36","3","2","1"]},{"name": "SELLER_COUNT","coef": -6.499,"values": ["25","13","21","8","14","5","6","7","4","50","51","41","47","43","45","46","44","40","26","37","52","39","30","29","33","32","0","34","36","3","2","1"]},{"name": "SELLER_COUNT","coef": 22.3559,"values": ["1"]},{"name": "SELL_VOLUME_NUMBER","coef": 0,"values": []}],"coef": -11.215};
 //////////////////////////////////////////////////////
 function loadData() {
 	var realm = getRealm();
@@ -200,28 +201,57 @@ function loadData() {
 			if(suitable){
 				preditctionParams['wealth_index'] = val.wi;
 				preditctionParams['education_index'] = val.ei;
-				preditctionParams['average_salary'] = val.as;
 				preditctionParams['market_index'] = val.mi;
 				preditctionParams['market_volume'] = val.v;
 				preditctionParams['local_percent'] = val.lpe;
-				preditctionParams['local_price'] = val.lpr;
-				preditctionParams['local_quality'] = val.lq;
-				preditctionParams['shop_size'] = $('#shopSize').val();
-				preditctionParams['town_district'] = $('#townDistrict').val();
-				preditctionParams['department_count'] = parseFloat($('#departmentCount').val());
-				preditctionParams['brand'] = parseFloat($('#brandFrom').val());
 				preditctionParams['price'] = parseFloat($('#priceFrom').val());
-				preditctionParams['quality'] = parseFloat($('#qualityFrom').val());
-				preditctionParams['notoriety'] = parseFloat($('#notoriety').val());
-				preditctionParams['visitors_count'] = $('#visitorsСount').val();
-				preditctionParams['service_level'] = $('#serviceLevel').val();
-				preditctionParams['seller_count'] = val.sc;
 				
+				var price = coefficients.coef;
+				for(var a = 0; a < coefficients.attrs.length; ++a){
+				  if(coefficients.attrs[a].values.length === 0){
+				    price += coefficients.attrs[a].coef;
+				  } else {
+				    var value = 0;
+				    switch(coefficients.attrs[a].name){
+					    case 'AVERAGE_SALARY': value = val.as;
+					    break;
+					    case 'LOCAL_PRICE': value = val.lpr;
+					    break;
+					    case 'LOCAL_QUALITY': value = val.lq;
+					    break;
+					    case 'SHOP_SIZE': value = $('#shopSize').val();
+					    break;
+					    case 'TOWN_DISTRICT': value = $('#townDistrict').val();
+					    break;
+					    case 'DEPARTMENT_COUNT': value = parseFloat($('#departmentCount').val());
+					    break;
+					    case 'BRAND': value = parseFloat($('#brandFrom').val());
+					    break;
+					    case 'QUALITY': value = parseFloat($('#qualityFrom').val());
+					    break;
+					    case 'NOTORIETY': value = parseFloat($('#notoriety').val());
+					    break;
+					    case 'VISITORS_COUNT': value = $('#visitorsСount').val();
+					    break;
+					    case 'SERVICE_LEVEL': value = $('#serviceLevel').val();
+					    break;
+					    case 'SELLER_COUNT': value = val.sc;
+					    break;
+					    case 'SELL_VOLUME_NUMBER': value = parseFloat(val.v) * 0.1; //10%
+					    break;
+				    }
+				    for(var v = 0; v < coefficients.attrs[a].values.length; ++v){
+				      if(value == coefficients.attrs[a].values[v]){
+					price += coefficients.attrs[a].coef;
+				      }
+				    }
+				  }
+				}
 				output += '<tr class="trec">';
 				output += '<td id="td_city"><a target="_blank" href="http://virtonomica.ru/'+realm+'/main/globalreport/marketing/by_trade_at_cities/'+val.pi+'/'+val.ci+'/'+val.ri+'/'+val.ti+'">'+val.tc+'</a></td>';
-				output += '<td align="right" id="td_volume_set">'+predictCommonBySet()+'</td>';
-				output += '<td align="right" id="td_volume_cv">'+predictCommonByCV()+'</td>';
-				output += '<td align="right" id="td_volume_perc_set">'+''+'</td>';
+				output += '<td align="right" id="td_volume_set">'+price+'</td>';
+				output += '<td align="right" id="td_volume_cv">'+val.spr+'</td>';
+				output += '<td align="right" id="td_volume_perc_set">'+val.lpr'+'</td>';
 				output += '<td align="right" id="td_volume_perc_cv">'+''+'</td>';
 				output += '</tr>';
 			}
